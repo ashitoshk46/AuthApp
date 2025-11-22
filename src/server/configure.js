6a83b1de97854948df403ed221bf7cb403743821
Ashitosh Ananda Kamble <ashitosh.kamble46@gmail.com>
2025-11-22 20:11:34 +0530

    updated configure to update logic of trimming of process.env.NODE_ENV, which seem to add extra space at end if set using command while starting app from script. Also added new EVN VAR process.env.APP_URL and process.env.HOST which will have different values in dev and prod.

--- src/server/configure.js
+++ src/server/configure.js
@@ -4,12 +4,21 @@ const configure = async () => {
     // Load .env in non-production
     console.log();
     console.log('=> Loading environment variables...');
+
+    
+    process.env.NODE_ENV = process.env.NODE_ENV?.trim();
+
     if (process.env.NODE_ENV !== 'production') {
         await import('dotenv').then(dotenv => {
             dotenv.config();
         });
+        process.env.NODE_ENV =  process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+        process.env.APP_URL = `${process.env.HOST}:${process.env.PORT}`;
+    } else {
+        process.env.APP_URL = process.env.HOST;
     }
-    console.log('>> Environment variables loaded.');
+
+    console.log('>> Environment variables loaded');
 
     initializeDb();
 }
