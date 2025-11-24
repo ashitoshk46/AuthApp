676a872a5e6997e5a58ac832605a09c437286842
Ashitosh Ananda Kamble <ashitosh.kamble46@gmail.com>
2025-11-24 14:33:31 +0530

    Update code to add vercel support

--- src/server.js
+++ src/server.js
@@ -5,25 +5,31 @@ import appRoutes from "./server/addRoute.js";
 import runPreChecks from "./server/runPreChecks.js";
 
 const startServer = async () => {
-  
+
   await configure();
-  
-  await runPreChecks();
-  
+
+  if (process.env.LOADER != 'vercel')
+    await runPreChecks();
+
   const app = generateApp();
   appRoutes(app);
-  
+
   console.log();
   console.log("<<===============================>>");
   console.log();
-  
+
   const port = process.env.PORT || 3000;
-  app?.listen(port, () => console.log(`Starting server in ${process.env.NODE_ENV} mode on port ${port}`));
+  if (process.env.LOADER != 'vercel')
+    app?.listen(port, () => console.log(`Starting server in ${process.env.NODE_ENV} mode on port ${port}`));
 
   process.on('SIGTERM', () => {
     console.log('Shutting down gracefully...');
     process.exit(0);
   });
+
+  return app;
 }
 
-startServer();
+const app = await startServer();
+
+export default app;
